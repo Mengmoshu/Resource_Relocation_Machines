fd6b6765bfd6de51077146e581966df379a56c35
TheStaplergun <staplergun@gmail.com>
2019-06-22 22:13:42 -0400

    Queue

--- scripts/machine.lua
+++ scripts/machine.lua
@@ -1,92 +1,107 @@
-local helper = require "helper"
+local Q = require('rmm-queue')
 
-local m = {}
-local typeJump = {
-    -- key = entity.name, value = function
-    "rrm-range10-building" = ResourceRelocationMachine,
-    "rrm-range20-building" = ResourceRelocationMachine,
-    "rrm-range30-building" = ResourceRelocationMachine
+local range_table = {
+    ["rrm-range10-building"] = 10,
+    ["rrm-range20-building"] = 20,
+    ["rrm-range30-building"] = 30
 }
 
-function ResourceRelocationMachine(entity) -- RRM specific ticking
 
+local function build_search_position(machine_dir, mpx,mpy, i)
+    return {
+        x = machine_dir == 2 and mpx + i or machine_dir == 6 and mpx - i or mpx,
+        y = machine_dir == 0 and mpy - i or machine_dir == 4 and mpy + i or mpy
+    }
 end
 
-function m.tick(bucket)
-    for k, v in pairs(bucket) do
-        local name = v.name
-        -- Function table magic?
-        typeJump.[name](v)
+
+local function find_closest(machine_dir, machine_pos, machine_name, machine_surface)
+    local distance = range_table[machine_name]
+    local find = machine_surface.find_entities_filtered
+    local mpx, mpy = machine_pos.x, machine_pos.y
+    for i = 1, distance do
+        local search_position = build_search_position(machine_dir, mpx, mpy, i)
+        local resource = find({position = search_position, type = "resource"})[1]
+        if resource then
+            return resource
+        end
     end
 end
 
--- Performance Queue Notes:
--- Write new version of this function to take a Bucket table, named m.tick()
--- Refactor machine class specific stuff into seperate function
--- Rename this function to processMachines(bucket)
-function m.processRRMs()
-    for k, RRM in pairs(global.RRMs) do
-    
-        if RRM.valid then -- Check to see if the RRM is there
-            local range = machine.setRange(RRM.name)
-            local infront = RRM.direction
-            local behind = (RRM.direction + 4) % 8
-            local signal = nil
-            
-            if myDebug == true then
-                RRM.surface.print("processRRMs fired, RRM.valid true")
-                RRM.surface.print(RRM.direction)
-            end
-            
-            -- Search behind RRM for ore
-            for k = 1, range + 1 do
-                test = RRM.surface.find_entities_filtered({area = {helper.searchArea(RRM, behind, k)}, type = "resource"}) -- Tile for ore
-                if test ~= nil then -- If the list is not empty something was found, time to work
-                    for k, xx in pairs(test) do -- Iterate over the (Hopefully small) list of found resources
-                        if xx.name ~= "crude-oil" then -- We don't relocate oil patches
-                            signal = xx
-                            break
-                        end
-                    end
-                end
-                if signal ~= nil then
-                    break -- Drop out of search loop to let teleport loop have its turn
-                end
-            end
-            
-            -- Only do this if signal is found and/or there's an ore entity to work with
-            if signal ~= nil then
-                -- Find suitable destination
-                for n = 1, range + 1 do
-                    dest = RRM.surface.find_entities_filtered({area = {helper.searchArea(RRM, infront, n)}, type = "resource"}) -- We only need there to be no entities of type "resource"
-                    if next(dest) == nil then
-                        signal.teleport({helper.searchDirection(RRM, infront, n).x, helper.searchDirection(RRM, infront, n).y})
-                        -- Set signal variable to successful ore move
-                        break -- Goes out one for loop
-                    end
+local function op_dir(d)
+    return (d+4)%8
+end
+local function find_open_position(machine_dir, machine_pos, machine_name, machine_surface, res_name)
+    machine_dir = op_dir(machine_dir) --Have to look behind the machine for an open position
+    local distance = range_table[machine_name]
+    local find = machine_surface.find_non_colliding_position
+    local mpx, mpy = machine_pos.x, machine_pos.y
+    for i = 1, distance do
+        local search_position = build_search_position(machine_dir, mpx, mpy, i)
+        local empty_spot = find(res_name, search_position, 0.5, 1, true)
+        if empty_spot then
+            return empty_spot
+        end
+    end
+end
+
+
+
+local function process_queue(q_index, q_bin)
+    for i,bin_slot in pairs(q_bin) do --Iterate over every entry in this array.
+        local entity = bin_slot.entity
+        if entity.valid then            --Verify entity is valid or remove it from the queue.
+            local entity_dir = entity.direction
+            local entity_pos = entity.position
+            local entity_name = entity.name
+            local entity_surface = entity.surface
+            local resource = find_closest(entity_dir, entity_pos, entity_name, entity_surface)
+            if resource then
+                local closest_position = find_open_position(entity_dir, entity_pos, entity_name, entity_surface, resource.name)
+                if closest_position then
+                    resource.teleport(closest_position)
                 end
             end
         else
-            table.remove(global.RRMs, k) -- Remove missing RRM
-            if #global.RRMs == 0 then
-                global.RRMs = nil -- Nil the table so ticker will stop
-            end
+            local data = {
+                q_index = q_index,
+                q_bin_slot = i
+            }
+            global.machines[bin_slot.unit_number] = nil
+            Q.r(data)
         end
-    
     end
 end
 
-
-function m.setRange(name) -- Done AFAIK
-    if name == "rrm-range10-building" then
-        return 10
-    elseif name == "rrm-range20-building" then
-        return 20
-    elseif name == "rrm-range30-building" then
-        return 30
-    else
-        return "Wat!?"
+local function on_tick()
+    local q = global.queue
+    local q_index = (game.tick % 60) + 1
+    local q_bin = q[q_index]
+    if q_bin and next(q_bin) then           --Ensures that the current bin exists and is not empty
+        process_queue(q_index, q_bin)
     end
 end
 
-return m
+local function initialize_rmm_machine(event)
+    local entity = event.created_entity or event.entity
+    local entity_name = entity.name
+    if range_table[entity_name] then --Check if valid entity to work with
+        if not global.running then   --Initialize the queue if it's not running already.
+            global.queue = {}
+            global.running = true
+            script.on_event(defines.events.on_tick, on_tick)
+        end
+        local data = Q.a(entity)    --Store the entity in the queue
+        global.machines = global.machines or {}
+        global.machines[entity.unit_number] = data      --Table returned from Q.a stores bin/bin_slot under the entities unit number. This allows you to remove it in the event it's invalid later in the script
+    end
+end
+script.on_event(defines.events.on_built_entity, initialize_rmm_machine)
+script.on_event(defines.events.on_robot_built_entity, initialize_rmm_machine)
+
+local function on_load()
+    if global.running then
+        script.on_event(defines.events.on_tick, on_tick)
+    end
+end
+script.on_load(on_load)
